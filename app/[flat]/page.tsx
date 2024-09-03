20fb034ce47f5a95e7a0af0c83d21a9f74183dc8
Danil Alshanov <danil@Danil-MacBook-Pro.local>
2024-09-03 18:58:44 +1000

    add booking button

--- app/[flat]/page.tsx
+++ app/[flat]/page.tsx
@@ -15,32 +15,41 @@ export default function FlatPage() {
     setBusy(flat?.id! > 3);
     setColor(
       flat?.id! > 3
-        ? " from-green-300 to-green-200"
-        : " from-red-300 to-red-200"
+        ? " from-green-300 to-green-200 text-green-800"
+        : " from-red-300 to-red-200 text-red-800"
     );
   }, [params, flat]);
   return (
-    <div className="text-xl text-black mx-4">
-      <h1 className="font-bold tracking-wide">{flat?.title}</h1>
+    <div className="text-xl text-black">
+      <h1 className="mx-4 font-bold tracking-wide">{flat?.title}</h1>
       <div
         className={
-          "my-2 text-center rounded-md py-2 uppercase tracking-wider bg-gradient-to-b " +
+          "my-2 text-center py-2 uppercase tracking-wider bg-gradient-to-b " +
           color
         }
       >
         {busy ? "Занята" : "Свободна"}
       </div>
-      {busy ? (
-        <div>
-          <p>Текущая бронь:</p>
-          <p>Живут: Новолекс</p>
-          <p>До: 5 Сентября 2024</p>
-        </div>
-      ) : (
-        <div>
+      <div className="mx-4">
+        {busy ? (
+          <>
+            <p className="text-center my-2">Текущая бронь</p>
+            <p>Живут: Новолекс</p>
+            <p>До: 5 Сентября 2024</p>
+            <p>
+              Освобождается через{" "}
+              <span className="font-bold">{flat?.id + " дня"}</span>
+            </p>
+          </>
+        ) : (
           <p>Квартира свободна</p>
-        </div>
-      )}
+        )}
+      </div>
+      <div className="mt-4 flex justify-center">
+        <button className="text-slate-800 border border-slate-700 bg-gradient-to-b from-slate-100 to-slate-300 py-2 px-4 rounded-md shadow-md active:shadow-inner">
+          Новая бронь
+        </button>
+      </div>
     </div>
   );
 }
