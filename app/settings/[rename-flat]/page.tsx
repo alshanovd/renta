e081f1039e032d2bec85f426fbbd9d7cb4a35fac
Danil Alshanov <danil@Danil-MacBook-Pro.local>
2024-09-06 00:58:44 +1000

    rename-flat comp fix

--- app/settings/[rename-flat]/page.tsx
+++ app/settings/[rename-flat]/page.tsx
@@ -1,26 +1,3 @@
-"use client";
-import Button from "@/components/button";
-import { Flat } from "@prisma/client";
-import { useState } from "react";
-
-export default function RenameFlat({
-  flat,
-  onRename,
-}: {
-  flat: Flat;
-  onRename: (newTitle: string) => void;
-}) {
-  const [newTitle, setNewTitle] = useState(flat.title);
-
-  return (
-    <div>
-      <input
-        type="text"
-        className="border py-1 px-2"
-        value={newTitle}
-        onChange={(e) => setNewTitle(e.target.value)}
-      />
-      <Button onClick={() => onRename(newTitle)}>Rename</Button>
-    </div>
-  );
+export default function RenameFlat() {
+  return <div className="text-black">Переименовать квартиру</div>;
 }
